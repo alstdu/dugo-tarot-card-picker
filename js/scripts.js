2443e37985238f797750698227d1604fb7f38db5
alstdu <lexi.dugo@gmail.com>
2024-01-23 20:34:04 -0800

    Add comment outlining plan for project

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+/**
+ * Set up some an array of objects that represent cards in a deck.
+ *  They should contain a cardId, an image path, and a list of keyword strings.
+ * Set up a function that chooses a random card from the list
+ * Set up a function that chooses two or three random cards from the list
+ *  To do this, select one number between 0 and the deck size,
+ *  then select another from 0 to the deck size minus 1.
+ *  Then check if the second number is equal to or greater than the first number.
+ *  If it is, add 1 to the second number.
+ *  You can extrapolate this process for selecting three cards.
+ * Set up functions for interacting with the dom.
+ *  Add event listeners to buttons.
+ *  Add a function to populate the image src when a card is selected.
+ *  Add a function to clear the cards from the screen.
+ *  Add functions to change which buttons are shown on screen depending on state.
+ */
