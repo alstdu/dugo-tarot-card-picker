3f42c9b5dd1cbfc71495261e2fd9a20424048537
alstdu <lexi.dugo@gmail.com>
2024-01-24 14:41:27 -0800

    Add 77 more templates and add cardIds

--- js/scripts.js
+++ js/scripts.js
@@ -21,4 +21,389 @@ const tarotCards = [
         keywords: [],
         imagePath: '',
     },
+    {
+        cardId: 1,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 2,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 3,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 4,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 5,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 6,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 7,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 8,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 9,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 10,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 11,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 12,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 13,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 14,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 15,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 16,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 17,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 18,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 19,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 20,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 21,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 22,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 23,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 24,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 25,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 26,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 27,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 28,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 29,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 30,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 31,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 32,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 33,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 34,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 35,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 36,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 37,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 38,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 39,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 40,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 41,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 42,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 43,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 44,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 45,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 46,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 47,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 48,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 49,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 50,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 51,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 52,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 53,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 54,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 55,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 56,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 57,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 58,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 59,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 60,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 61,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 62,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 63,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 64,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 65,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 66,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 67,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 68,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 69,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 70,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 71,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 72,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 73,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 74,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 75,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 76,
+        keywords: [],
+        imagePath: '',
+    },
+    {
+        cardId: 77,
+        keywords: [],
+        imagePath: '',
+    },
 ];
