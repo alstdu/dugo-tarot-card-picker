1599658d920a1029f3bcaffa5d581b4b853ad28f
alstdu <lexi.dugo@gmail.com>
2024-01-27 16:13:49 -0800

    Add initial tarot card setup and random card selection function

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 /**
  * Set up some an array of objects that represent cards in a deck.
- *  They should contain a cardId, an image path, and a list of keyword strings.
+ *  They should contain a title, an image path, and a list of keyword and reverse keyword strings.
  * Set up a function that chooses a random card from the list
  * Set up a function that chooses two or three random cards from the list
  *  To do this, select one number between 0 and the deck size,
@@ -14,3 +14,44 @@
  *  Add a function to clear the cards from the screen.
  *  Add functions to change which buttons are shown on screen depending on state.
  */
+
+// TODO: should be in a separate file
+const tarotCards = [
+    {
+        title: '1',
+        uprightKeywords: [],
+        reverseKeywords: [],
+        imagePath: '',
+    },
+    {
+        title: '2',
+        uprightKeywords: [],
+        reverseKeywords: [],
+        imagePath: '',
+    },
+    {
+        title: '3',
+        uprightKeywords: [],
+        reverseKeywords: [],
+        imagePath: '',
+    },
+    {
+        title: '4',
+        uprightKeywords: [],
+        reverseKeywords: [],
+        imagePath: '',
+    },
+    {
+        title: '5',
+        uprightKeywords: [],
+        reverseKeywords: [],
+        imagePath: '',
+    },
+];
+
+const chooseACard = () => {
+    const randomIndex = Math.floor( Math.random() * tarotCards.length );
+    return tarotCards[randomIndex];
+};
+
+console.log( chooseACard() );
